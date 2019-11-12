df10cd294b564dae653927d08f277d652ec1cfd1
Skyproject12 <sumendrakadek108@gmail.com>
2019-11-12 23:31:11 +0700

    Parse JSON

--- script.js
+++ script.js
@@ -10,3 +10,16 @@
 // // megubah object ke json
 // console.log(JSON.stringify(mahasiswa));
 
+let xhr = new XMLHttpRequest(); 
+xhr.onreadystatechange= function(){  
+    // check request status 
+    if(xhr.readyState==4 && xhr.status==200){
+        // call response text and tampung in the mahasiswa variable  
+        let mahasiswa= JSON.parse(this.responseText);  
+        // execution in console
+        console.log(mahasiswa);
+    }
+} 
+// to get file json use js
+xhr.open('GET','coba.json', true); 
+xhr.send();
